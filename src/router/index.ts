a7fb6e5546065da70d15585495aaf2e0125fa8e2
kinnaamf <dimoje.1337@gmail.com>
2026-06-19 05:11:06 +0300

    chore: some project configuration

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,8 +1,11 @@
 import { createRouter, createWebHistory } from 'vue-router'
+import Home from "@/views/Home.vue";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [],
+  routes: [
+    { path: '/', component: Home, meta: { title: 'Deadstat' } },
+  ],
 })
 
 export default router
