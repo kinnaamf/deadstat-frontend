7607d21a2442fccf563afba70e73cf2319b80d8d
kinnaamf <dimoje.1337@gmail.com>
2026-07-07 11:17:07 +0300

    chore: rename Home -> HomeView

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,10 +1,10 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import Home from "@/views/Home.vue";
+import HomeView from "@/views/HomeView.vue";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
-    { path: '/', component: Home, meta: { title: 'Deadstat' } },
+    { path: '/', component: HomeView, meta: { title: 'Deadstat' } },
   ],
 })
 
