ac061a7ad644e6136cd3ee9417cb995e9519f184
kinnaamf <dimoje.1337@gmail.com>
2026-07-07 16:02:04 +0300

    style: add routes to router

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,10 +1,14 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from "@/views/HomeView.vue";
+import HeroStatsView from "@/views/HeroStatsView.vue";
+import MainStatsView from "@/views/MainStatsView.vue";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     { path: '/', component: HomeView, meta: { title: 'Deadstat' } },
+    { path: '/player-stats', component: HeroStatsView, meta: { title: 'Hero stats' } },
+    { path: '/main-stats',  component: MainStatsView, meta: { title: 'MainStats' } },
   ],
 })
 
